0019ab62cdbd2c7c5f12257844b3f19b24ab7e18
mystique09 <knightkrypto7@gmail.com>
2021-08-08 01:37:47 +0800

    made few chages

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,45 +1,29 @@
 const router = require('express').Router();
 const User = require('../models/user.model');
 const userController = require('../controllers/user.controller')
-const hashPassword = require('../utils/hashPassword');
+const authUser = require('../utils/authUser');
 
-router.get('/', function(req, res){
-  const users = User.find().select('username').limit(5);
-  return res.json(users);
-})
+router.get('/', authUser, async function(req, res) {
+  const users = await userController.getUsers(5);
+  return res.json({
+    users
+  });
+});
 
-router.post('/sign-up', async function(req, res){
-  const {username, password} = req.body;
-  
-  if(!username || !password){
-    return res.json({error: 'Please complete the missing fields.'});
-  }
+router.delete('/', authUser, async function(req, res) {
+  const {_id} = req.user;
   
   try {
+    const isDeleted = await User.findOneAndRemove({
+      _id
+    });
     
-    const userCheck = await User.findOne({username});
+    if(!isDeleted) return res.status(404).json({error: 'Invalid account.'});
     
-    if(userCheck)throw new Error('User already exist!');
-    
-    const newUser = new User({username, password});
-    const saved = await newUser.save();
-    
-    if(saved){
-      return res.status(200).json({success: 'Account added.'});
-    }
+    return res.status(200).json({success: 'Account sucfessfuly deleted.'});
   } catch (e) {
-    return res.json({error: e.message});
+    return res.status(404).json({error: e.message});
   }
-});
-
-router.post('/sign-in', function(req, res){
-  const {username, password} = req.body;
-  
-  if(!us || !password)return res.json({error: 'Please complete the missing fields.'});
-});
-
-router.post('/sign-out', function(req, res){
-  
 });
 
 module.exports = router;
